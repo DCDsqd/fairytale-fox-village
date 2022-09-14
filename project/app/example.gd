83dbef42f03122952ba43a068eb3c4dc92f4cbd9
storst1 <kokakolazzz100@mail.ru>
2022-09-15 00:16:59 +0300

    Updating game script to handle time control

--- project/app/example.gd
+++ project/app/example.gd
@@ -4,3 +4,30 @@ func _ready():
 	var game := Game.new()
 	print(game.test_godot_output())
 	print("Starting time: ", str(game.current_time()) + ":00", ", starting day: ", str(game.current_day()))
+	_setup_time_control(game)
+
+#Starts the timer for updating game time during the game
+func _setup_time_control(game:Game) -> void:
+	# Timer node
+	var timer := Timer.new()
+
+	# Set timer interval in seconds
+	var HOUR_REAL_TIME := 0.5
+	timer.set_wait_time(HOUR_REAL_TIME)
+
+	# Set it as repeat
+	timer.set_one_shot(false)
+
+	# Connect timer to add_hour function
+	timer.connect("timeout", self, "add_hour", [game])
+
+	# Add to the tree as child of the current node
+	add_child(timer)
+	
+	#Start the timer
+	timer.start()
+
+#Adds an hour to C++ game class time property and prints updated time
+func add_hour(game:Game) -> void:
+	game.add_hour()
+	print("Time has been updated! New time: ", str(game.current_time()) + ":00", ", day: ", str(game.current_day()))
