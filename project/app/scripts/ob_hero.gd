c6c4d43c928732c73d384616262efb54fdea7f6b
Nekto112 <Nekto112@users.noreply.github.com>
2022-09-19 23:07:33 +0300

    fox solid collision

--- project/app/scripts/ob_hero.gd
+++ project/app/scripts/ob_hero.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends KinematicBody2D
 
-var speed = 100
+var speed = 5000
 var _x = 0
 var _y = 0
 var anim = 'f_idle'
@@ -33,5 +33,6 @@ func _process(delta) -> void:
 	_x = int(Input.is_action_pressed("mv_right")) - int(Input.is_action_pressed("mv_left"))
 	_y = int(Input.is_action_pressed("mv_down")) - int(Input.is_action_pressed("mv_top"))
 	get_node('spr').play(anim)
+	
 	if (true):
-		position = position.move_toward(Vector2(position.x+_x,position.y+_y), delta * speed)
+		move_and_slide(Vector2(_x,_y) * delta * speed)
