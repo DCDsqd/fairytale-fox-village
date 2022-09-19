b5914fc11c8122b7f76b0a002e800c6121d1aabc
Nekto112 <Nekto112@users.noreply.github.com>
2022-09-19 23:08:37 +0300

    Revert "Merge branch 'main' of https://github.com/DCDsqd/fairytale-fox-village"
    This reverts commit 2bcd998896a12a2dc0c294e27f479fc586e93e77, reversing
    changes made to c6c4d43c928732c73d384616262efb54fdea7f6b.

--- project/src/game/Food.hpp
+++ project/src/game/Food.hpp
@@ -1,27 +1,14 @@
 #pragma once
 
 #include <vector>
-#include <set>
-#include <memory>
 
 #include <Godot.hpp>
-#include <Node.hpp>
 
 #include "Ingridient.hpp"
 
 namespace godot {
 
-class Food : public Node {
-
-	GODOT_CLASS(Food, Node);
-
-public:
-    static void _register_methods()
-    {
-        //register_method("", &Food);
-    }
-
-    void _init() {};
+class Food {
 
 private:
 	godot::String name;
--- project/src/godot/Godot.cpp
+++ project/src/godot/Godot.cpp
@@ -28,7 +28,6 @@ namespace godot
         godot::register_class<godot::Player>();
         godot::register_class<godot::Centrifuge>();
         godot::register_class<godot::Ingridient>();
-        godot::register_class<godot::Food>();
     }
 
 } //namespace godot
