dc6a8b54b84a6f76167fe6afeafeb7d2269ead11
storst1 <kokakolazzz100@mail.ru>
2022-09-21 02:29:55 +0300

    Adjusting food class to be used in gdscript

--- project/src/game/Food.hpp
+++ project/src/game/Food.hpp
@@ -1,19 +1,44 @@
 #pragma once
 
 #include <vector>
+#include <set>
+#include <memory>
 
 #include <Godot.hpp>
+#include "Node.hpp"
 
 #include "Ingridient.hpp"
 
 namespace godot {
 
-class Food {
+class Food : public Node {
+
+	GODOT_CLASS(Food, Node);
+
+public:
+	static void _register_methods()
+	{
+		//register_method("", &Food::);
+		register_method("bind_values", &Food::bind_values);
+		register_method("get_name", &Food::get_name);
+		register_method("get_id", &Food::get_id);
+		register_method("get_ingridients", &Food::get_ingridients);
+		register_method("get_type", &Food::get_type);
+	}
+
+	void _init() {};
+
+	void bind_values(int _id, godot::String _name, godot::Array ingr_reqs, godot::String _type);
+	inline godot::String get_name() const { return name; };
+	inline int get_id() const { return id; };
+	godot::Array get_ingridients();
+	inline godot::String get_type() const { return type; };
 
 private:
 	godot::String name;
 	int id;
-	std::vector<Ingridient> ingridients;
+	godot::String type;
+	std::vector<Ingridient*> ingridients;
 };
 
 } //namespace godot
--- project/src/game/GameData.hpp
+++ project/src/game/GameData.hpp
@@ -27,7 +27,11 @@ public:
 		register_method("total_civilians", &GameData::total_civilians);
 		register_method("add_ingridient", &GameData::add_ingridient);
 		register_method("get_ingridient", &GameData::get_ingridient);
+		register_method("get_ingridient_mult", &GameData::get_ingridient_mult);
 		register_method("total_ingridients", &GameData::total_ingridients);
+		register_method("add_food", &GameData::add_food);
+		register_method("get_food", &GameData::get_food);
+		register_method("total_foods", &GameData::total_food);
 	}
 
 	void _init() {};
@@ -38,19 +42,26 @@ public:
 	inline int total_ingridients() const {
 		return (int)all_ingridients.size();
 	};
+	inline int total_food() const {
+		return (int)all_food.size();
+	};
 
 	void add_civilian(Civilian* civ);
 	Civilian* get_civilian(int idx);
 
 	void add_ingridient(Ingridient* ingr);
 	Ingridient* get_ingridient(int idx);
+	godot::Array get_ingridient_mult(godot::Array idxs);
+
+	void add_food(Food* food);
+	Food* get_food(int idx);
 
 private:
 	std::vector<Civilian*> all_civilians;
 	std::vector<Dialog*> all_dialogs;
 	std::vector<Ingridient*> all_ingridients;
 	std::vector<Quest*> all_quests;
-	std::vector<const Food*> all_food;
+	std::vector<Food*> all_food;
 };
 
 }
