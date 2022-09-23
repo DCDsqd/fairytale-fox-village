901d70e051f8898439fc5b9295d24756d22d1c84
Nekto112 <Nekto112@users.noreply.github.com>
2022-09-23 05:49:55 +0300

    shop_work_mb

--- project/app/objs/npc/ob_shopkeeper.gd
+++ project/app/objs/npc/ob_shopkeeper.gd
@@ -4,8 +4,8 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var hero = get_parent().get_parent().get_node("ob_hero")
-onready var craft_ui : HBoxContainer;
+onready var hero = get_parent().get_node("ob_hero")
+onready var craft_ui : Control = get_parent().get_node("camera_street/Control");
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	z_index = position.y/5
